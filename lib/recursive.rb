321f7e2eafe81365325df4033253ba7c9d15a7d2
nikaraa <giuseppemalara00001@gmail.com>
2023-05-02 01:32:29 +0200

    Add fibonacci.rb and fibs_rec recursive method

---
+++ lib/recursive.rb
@@ -0,0 +1,24 @@
+def fibs(n)
+  array = []
+  for i in 0..n
+    if i == 0
+      array.push(0)
+    elsif i == 1
+      array.push(1)
+    else
+      first = array[i - 1]
+      second = array[i - 2]
+      array.push(first + second)
+    end
+    i += 1
+  end
+  return array
+end
+
+def fibs_rec(n)
+  return [0, 1] unless n > 1
+  num = fibs_rec(n - 1)
+  num.push(num[-1] + num[-2])
+end
+
+p fibs_rec(15)
